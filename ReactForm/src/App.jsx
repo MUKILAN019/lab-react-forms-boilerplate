5d17e5e73a059d78a8d593b8afa5fc7f1b3ecd82
MUKILAN019 <mukilan.p@kalvium.community>
2024-02-27 21:38:53 +0530

    added files

--- ReactForm/src/App.jsx
+++ ReactForm/src/App.jsx
@@ -16,11 +16,11 @@ function App(){
         phoneno: "",
     })
 
-const handleChange = (event) => {
-    const inputValue = event.target.value
-    const inputField = event.target.name
-    setState({...state, [inputField]: inputValue})
-}
+    const handleChange = (event) => {
+        const inputValue = event.target.value
+        const inputField = event.target.name
+        setState({...state, [inputField]: inputValue})
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -30,37 +30,50 @@ const handleChange = (event) => {
         if(email === "" || phoneno === "" || email.includes(" ")){
             setWholeError(true)
             return;
-       }
-       if(firstname === ""){
-        return setFirstnameError(true)
-       }
-       if(lastname === ""){
-        return setLastnameError(true)
-       }
-       if(phoneno.length !== 10){
-        return setPhoneError(true)
-       }
-       if(!email.includes("@kalvium.community")){
-        return setEmailError(true)
-       }
-
-       let symbols = './,<>:;[]{}-=+*!@#$%^&*()|~Ee';
-       for(let i=0; i<10; i++){
-        if(symbols.includes(phoneno[i])){
-            return setPhoneError(true)
         }
-       }
-
-       let EmailName = email.split("@")[0];
-
-       let smallCharacters = "abcdefghijklmnopqrstuvwxyz.";
-       for(let i=0; i<EmailName.length; i++){
-        if(!smallCharacters.includes(EmailName[i])){
-            return setEmailError(true)
+        if(firstname === ""){
+            setFirstnameError(true)
+            return
+        } else {
+            setFirstnameError(false) // This line resets the error once the user has corrected it
         }
-       }
-SetSuccess(true)
+        if(lastname === ""){
+            setLastnameError(true)
+            return
+        } else {
+            setLastnameError(false) // This line resets the error once the user has corrected it
+        }
+        if(phoneno.length !== 10){
+            setPhoneError(true)
+            return
+        } else {
+            setPhoneError(false) // This line resets the error once the user has corrected it
+        }
+        if(!email.includes("@kalvium.community")){
+            setEmailError(true)
+            return
+        } else {
+            setEmailError(false) // This line resets the error once the user has corrected it
+        }
 
+        let symbols = './,<>:;[]{}-=+*!@#$%^&*()|~Ee';
+        for(let i=0; i<10; i++){
+            if(symbols.includes(phoneno[i])){
+                setPhoneError(true)
+                return
+            }
+        }
+
+        let EmailName = email.split("@")[0];
+
+        let smallCharacters = "abcdefghijklmnopqrstuvwxyz.";
+        for(let i=0; i<EmailName.length; i++){
+            if(!smallCharacters.includes(EmailName[i])){
+                setEmailError(true)
+                return
+            }
+        }
+        SetSuccess(true)
     }
 
     return(
@@ -71,71 +84,40 @@ SetSuccess(true)
               <div className="registration">
                 <p>Registration Successfull</p>
               </div>
-            ) : (
-              ' '
-            )}
+            ) : null}
             
-<form onSubmit={handleSubmit}>
-    <div>
-    <input id="inpbox" type="text" placeholder="First Name" name="firstname" onChange={handleChange}/>
-    {
-        FirstnameError === true ? (
-            <p style={{ color: 'red', fontSize: '15px' }}>Do not keep FirstName empty!!</p>
-        ) : ("")
-    }
-    </div>
-
-   <div>
-   <input id="inpbox" type="text" placeholder="Last Name" name="lastname" onChange={handleChange} />
-   {
-    LastnameError === true ? (
-        <p style={{ color: 'red', fontSize: '15px' }}> Do not keep Lastname empty!!
-        </p>
-    ) : ("")
-   }
-   </div>
-
-   <div>
-   <input id="inpbox" type="email" placeholder="@kalvium.community" name="email" onChange={handleChange} />
-   {EmailError === true ? (
-                <p style={{ color: 'red', fontSize: '15px' }}>
-                  Enter correct format for email
-                </p>
-              ) : (
-                ''
-              )}
-
-   </div>
-
-<div>
-<input id="inpbox" type="number" placeholder="Phone Number" name="phoneno" onChange={handleChange} maxLength={10}/>
-{PhoneError === true ? (
-                <p style={{ color: 'red', fontSize: '15px' }}>
-                  Enter correct format for phone
-                </p>
-              ) : (
-                ''
-              )}
-
-</div>
-{
-    WholeError === true ? (
-        <p>Enter input fields correctly!!</p>
-    ) : ("")
-}
-
-
-<input type="submit" />
-
-</form>
-
-
-
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input id="inpbox" type="text" placeholder="First Name" name="firstname" onChange={handleChange}/>
+                    {FirstnameError === true ? (
+                        <p style={{ color: 'red', fontSize: '15px' }}>Do not keep FirstName empty!!</p>
+                    ) : null}
+                </div>
+                <div>
+                    <input id="inpbox" type="text" placeholder="Last Name" name="lastname" onChange={handleChange} />
+                    {LastnameError === true ? (
+                        <p style={{ color: 'red', fontSize: '15px' }}> Do not keep Lastname empty!! </p>
+                    ) : null}
+                </div>
+                <div>
+                    <input id="inpbox" type="email" placeholder="@kalvium.community" name="email" onChange={handleChange} />
+                    {EmailError === true ? (
+                        <p style={{ color: 'red', fontSize: '15px' }}> Enter correct format for email </p>
+                    ) : null}
+                </div>
+                <div>
+                    <input id="inpbox" type="number" placeholder="Phone Number" name="phoneno" onChange={handleChange} maxLength={10}/>
+                    {PhoneError === true ? (
+                        <p style={{ color: 'red', fontSize: '15px' }}> Enter correct format for phone </p>
+                    ) : null}
+                </div>
+                
+                <input type="submit" />
+            </form>
         </div>
         </div>
-
         </div>
     )
 }
 
-export default App
+export default App;
